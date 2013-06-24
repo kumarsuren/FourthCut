2ea797b73320d71a3eea1f819db0bfc27a4f750d
Kumar Suren <kumarsuren2@gmail.com>
2013-06-23 23:30:45 -0400

    test changes

--- com.maha.science.web/src/test/java/com/maha/science/account/VKUserServiceTest.java
+++ com.maha.science.web/src/test/java/com/maha/science/account/VKUserServiceTest.java
@@ -34,7 +34,7 @@ public class VKUserServiceTest {
 		// act
 		userService.initialize();
 		// assert
-		verify(accountRepositoryMock, times(2)).save(any(Account.class));
+		verify(accountRepositoryMock, times(3)).save(any(Account.class));
 	}
 
 	@Test
